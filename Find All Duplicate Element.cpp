362d1f8fb118f3fc21cb47488a10291fcbfea19e
Aniket Singh <2k23.csai2313328@gmail.com>
2026-04-23 16:21:46 +0530

    Create Find All Duplicate Element.cpp

---
+++ Find All Duplicate Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+       int n=nums.size();
+       unordered_map<int,int>map;
+       for(int i=0;i<n;i++){
+        map[nums[i]]++;
+       } 
+       vector<int>ans;
+       for(auto x:map){
+        if(x.second>=2){
+            ans.push_back(x.first);
+        }
+       }
+       return ans;
+    }
+};
