d91fb2676d5873fb3374f5192d75af9c7cb5e504
Aniket Singh <2k23.csai2313328@gmail.com>
2026-07-04 11:51:47 +0530

    Create STRIVERS_MergeIntervals.cpp

---
+++ STRIVERS_MergeIntervals.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+       // Optimal , TC = O(NlogN)+O(N)
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        for(int i=0;i<n;i++){
+            if(res.empty() or res.back()[1] < intervals[i][0]){
+                res.push_back(intervals[i]);
+            }
+            else{
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
