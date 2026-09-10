e9ab8c47ce48200506f10d5c2eeab7d01c145196
Aniket Singh <2k23.csai2313328@gmail.com>
2026-09-10 12:47:48 +0530

    Time: 3 ms (13.29%), Space: 12.6 MB (83.44%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // using 1D array
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int>dp = triangle[triangle.size()-1];
+
+        int  n = dp.size();
+
+        for(int i = triangle.size()-2;i>=0;i--){
+            for(int j = 0;j<triangle[i].size();j++){
+                dp[j] = triangle[i][j]+min(dp[j],dp[j+1]);
+            }
+        }
+        return dp.front();
+    }
+};
